50f58224fad022a84022170aca0980ffd33d3c30
Peter Hanley <phanley@temple.edu>
2015-03-20 08:28:25 -0400

    added core.data (and util, because of dependencies on util.OwnedWrapper). Had trouble with ipaddr as it's out of date, so I added the more up to date version to requirements.txt and made a note to test and check the change (the newer version had many breaking changes)

---
+++ mdid3/util/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.conf import settings
+from django.contrib.auth.models import Group
+
+
+class OwnedWrapperManager(models.Manager):
+    """
+    Allows retrieval of a wrapper object by specifying
+    """
+    def get_for_object(self, user, object=None, type=None, object_id=None):
+        try:
+            type = int(type)
+        except TypeError:
+            pass
+
+        obj, created = self.get_or_create(
+            user=user,
+            object_id=object and object.id or object_id,
+            content_type=object and OwnedWrapper.t(object.__class__) or type)
+        return obj
+
+
+class OwnedWrapper(models.Model):
+    """
+    Used to connect a user to an object, currently used for tagging purposes to
+    keep track of tag owners
+    """
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    object = generic.GenericForeignKey('content_type', 'object_id')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    taggeditem = generic.GenericRelation('tagging.TaggedItem')
+
+    objects = OwnedWrapperManager()
+
+    class Meta:
+        unique_together = ('content_type', 'object_id', 'user')
+        db_table = 'util_ownedwrapper'
+
+    @staticmethod
+    def t(model):
+        return ContentType.objects.get_for_model(model)
